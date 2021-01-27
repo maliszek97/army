5bdd715adb941eb0cb884a6c74d043b77095bb20
maliszek97 <77755339+maliszek97@users.noreply.github.com>
2021-01-28 00:48:56 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,7 +21,26 @@ $(document).ready(function () {
                              '<p>Hełm --- ' + value.helm + '</p>' + '<p>Noktowizor --- ' + value.noktowizor + '</p>' + '<p>Celownik laserowy --- ' + value.calownik_laserowy + '</p>' + '<p>HWS --- ' + value.hws + '</p>' + '<p>Latarka --- ' + value.latarka + '</p>' + '<p>Łoparka piechoty --- ' + value.lopatka + '</p>' + '<p>Plecak medyczny --- ' + value.plecak_medyczny + '</p>' + '<p>Okulary --- ' + value.okulary + '</p>' + '<p>Lornetka --- ' + value.lornetka + '</p>');
         })                       
         
-        
+        $(".soldier2").append(data.druzyna[1].stanowisko);
+        $(".soldier2img").attr("src", data.druzyna[1].image);
+        $("#s21").append(data.druzyna[1].nazwa_operacyjna);
+        $("#s22").append(data.druzyna[1].dlugosc_sluzby);
+        $("#s23").append(data.druzyna[1].natychmiastowa_gotowosc);
+        $("#s24").append(data.druzyna[1].rejon_rozlokowania);
+        $("#s25").append(data.druzyna[1].posiadana_bron);
+        $("#s26").append(data.druzyna[1].status_medyczny);
+        $("#s27").append(data.druzyna[1].status_zaszczepienia);
+        $("#s28").append(data.druzyna[1].id_karty);
+        $("#s29").append(data.druzyna[1].amunicja);
+        $(".adres2").append(data.druzyna[1].adres.ulica);                       
+        $.each(data.druzyna[1].adres, function(i, value){
+        $(".adres2").append('<p>ul. ' + value.ulica + ' ' + value.numer_bloku + '/' + value.numer_mieszkania + '<br />' + value.kod_pocztowy + ' ' + value.miejscowosc +'<br />' +
+                       '<strong>Koordynaty:</strong><br />' + value.dlugosc_geograficzna + ', ' + value.szerokosc_geograficzna + '<br /></p>');
+        })
+        $.each(data.druzyna[1].wyposazenie, function(i, value){
+        $(".wyposazenie2").append('<p>Maska przeciwgazowa --- ' + value.maska_przeciwgazowa + '</p>' + '<p>Kamizelka --- ' + value.kamizelka + '</p>' + 
+                             '<p>Hełm --- ' + value.helm + '</p>' + '<p>Noktowizor --- ' + value.noktowizor + '</p>' + '<p>Celownik laserowy --- ' + value.calownik_laserowy + '</p>' + '<p>HWS --- ' + value.hws + '</p>' + '<p>Latarka --- ' + value.latarka + '</p>' + '<p>Łoparka piechoty --- ' + value.lopatka + '</p>' + '<p>Plecak medyczny --- ' + value.plecak_medyczny + '</p>' + '<p>Okulary --- ' + value.okulary + '</p>' + '<p>Lornetka --- ' + value.lornetka + '</p>');
+        })         
 
     });
 });  
