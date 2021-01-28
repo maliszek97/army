50a2aed29d0cc110b01cc659fc7b9fea427ba418
maliszek97 <77755339+maliszek97@users.noreply.github.com>
2021-01-28 11:24:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,60 @@
-// nazywamy nasze cache- w zmiennej będzie wygodniej
-const MY_CACHE = 'cache-name';
-// w tej tablicy lądują wszystkie pliki, które chcemy dodać do cache
-const MY_FILES = [
-        '/css/style.css',
-        '/js/index.js',
-        '/icon/192.png",
-        '/icon/army-icon.jpg'
-];
+var CACHE_NAME = 'my_cache'
+var URLS = [                            // Add URL you want to cache in this list.
+  '/maliszek97.github.io/',                     // If you have separate JS/CSS files,
+  '/maliszek97.github.io/js/index.js',
+  '/maliszek97.github.io/css/style.js',
+  '/maliszek97.github.io/icon/192.png',
+  '/maliszek97.github.io/icon/army-icon.jpg',
+  'https://my-json-server.typicode.com/maliszek97/json-server/db'      
+]
 
-// instalujemy nasz service worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(MY_CACHE).then(function(cache) {
-      return cache.addAll(MY_FILES);
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
-  );
-});
-// po aktywacji chcę skasować wszystkie cache w naszej domenie, które nie są naszym cache (to opcjonalne)
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== MY_CACHE;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
     })
-  );
-});
-// strategia 'Network falling back to cache'
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
     })
-  );
-});
+  )
+})
