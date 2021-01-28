ae3a8dd3494a83f49bcd2287418dbb59aa56e7e1
maliszek97 <77755339+maliszek97@users.noreply.github.com>
2021-01-28 01:28:02 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,7 +40,49 @@ $(document).ready(function () {
         $.each(data.druzyna[1].wyposazenie, function(i, value){
         $(".wyposazenie2").append('<p>Maska przeciwgazowa --- ' + value.maska_przeciwgazowa + '</p>' + '<p>Kamizelka --- ' + value.kamizelka + '</p>' + 
                              '<p>Hełm --- ' + value.helm + '</p>' + '<p>Noktowizor --- ' + value.noktowizor + '</p>' + '<p>Celownik laserowy --- ' + value.calownik_laserowy + '</p>' + '<p>HWS --- ' + value.hws + '</p>' + '<p>Latarka --- ' + value.latarka + '</p>' + '<p>Łoparka piechoty --- ' + value.lopatka + '</p>' + '<p>Plecak medyczny --- ' + value.plecak_medyczny + '</p>' + '<p>Okulary --- ' + value.okulary + '</p>' + '<p>Lornetka --- ' + value.lornetka + '</p>');
-        })         
+        })
+        
+        $(".soldier3").append(data.druzyna[2].stanowisko);
+        $(".soldier3img").attr("src", data.druzyna[2].image);
+        $("#s31").append(data.druzyna[2].nazwa_operacyjna);
+        $("#s32").append(data.druzyna[2].dlugosc_sluzby);
+        $("#s33").append(data.druzyna[2].natychmiastowa_gotowosc);
+        $("#s34").append(data.druzyna[2].rejon_rozlokowania);
+        $("#s35").append(data.druzyna[2].posiadana_bron);
+        $("#s36").append(data.druzyna[2].status_medyczny);
+        $("#s37").append(data.druzyna[2].status_zaszczepienia);
+        $("#s38").append(data.druzyna[2].id_karty);
+        $("#s39").append(data.druzyna[2].amunicja);
+        $(".adres3").append(data.druzyna[2].adres.ulica);                       
+        $.each(data.druzyna[2].adres, function(i, value){
+        $(".adres3").append('<p>ul. ' + value.ulica + ' ' + value.numer_bloku + '/' + value.numer_mieszkania + '<br />' + value.kod_pocztowy + ' ' + value.miejscowosc +'<br />' +
+                       '<strong>Koordynaty:</strong><br />' + value.dlugosc_geograficzna + ', ' + value.szerokosc_geograficzna + '<br /></p>');
+        })
+        $.each(data.druzyna[2].wyposazenie, function(i, value){
+        $(".wyposazenie3").append('<p>Maska przeciwgazowa --- ' + value.maska_przeciwgazowa + '</p>' + '<p>Kamizelka --- ' + value.kamizelka + '</p>' + 
+                             '<p>Hełm --- ' + value.helm + '</p>' + '<p>Noktowizor --- ' + value.noktowizor + '</p>' + '<p>Celownik laserowy --- ' + value.calownik_laserowy + '</p>' + '<p>HWS --- ' + value.hws + '</p>' + '<p>Latarka --- ' + value.latarka + '</p>' + '<p>Łoparka piechoty --- ' + value.lopatka + '</p>' + '<p>Plecak medyczny --- ' + value.plecak_medyczny + '</p>' + '<p>Okulary --- ' + value.okulary + '</p>' + '<p>Lornetka --- ' + value.lornetka + '</p>');
+        })
+        
+        $(".soldier4").append(data.druzyna[3].stanowisko);
+        $(".soldier4img").attr("src", data.druzyna[3].image);
+        $("#s41").append(data.druzyna[3].nazwa_operacyjna);
+        $("#s42").append(data.druzyna[3].dlugosc_sluzby);
+        $("#s43").append(data.druzyna[3].natychmiastowa_gotowosc);
+        $("#s44").append(data.druzyna[3].rejon_rozlokowania);
+        $("#s45").append(data.druzyna[3].posiadana_bron);
+        $("#s46").append(data.druzyna[3].status_medyczny);
+        $("#s47").append(data.druzyna[3].status_zaszczepienia);
+        $("#s48").append(data.druzyna[3].id_karty);
+        $("#s49").append(data.druzyna[3].amunicja);
+        $(".adres4").append(data.druzyna[3].adres.ulica);                       
+        $.each(data.druzyna[3].adres, function(i, value){
+        $(".adres4").append('<p>ul. ' + value.ulica + ' ' + value.numer_bloku + '/' + value.numer_mieszkania + '<br />' + value.kod_pocztowy + ' ' + value.miejscowosc +'<br />' +
+                       '<strong>Koordynaty:</strong><br />' + value.dlugosc_geograficzna + ', ' + value.szerokosc_geograficzna + '<br /></p>');
+        })
+        $.each(data.druzyna[3].wyposazenie, function(i, value){
+        $(".wyposazenie4").append('<p>Maska przeciwgazowa --- ' + value.maska_przeciwgazowa + '</p>' + '<p>Kamizelka --- ' + value.kamizelka + '</p>' + 
+                             '<p>Hełm --- ' + value.helm + '</p>' + '<p>Noktowizor --- ' + value.noktowizor + '</p>' + '<p>Celownik laserowy --- ' + value.calownik_laserowy + '</p>' + '<p>HWS --- ' + value.hws + '</p>' + '<p>Latarka --- ' + value.latarka + '</p>' + '<p>Łoparka piechoty --- ' + value.lopatka + '</p>' + '<p>Plecak medyczny --- ' + value.plecak_medyczny + '</p>' + '<p>Okulary --- ' + value.okulary + '</p>' + '<p>Lornetka --- ' + value.lornetka + '</p>');
+        })   
 
     });
 });  
